6c256203aa57ec8f3bc3020d6829a0f0e488cf69
KateliyaDharmen <kateliyadharmen333@gmail.com>
2024-01-12 21:52:17 +0530

    datatypes

---
+++ Basic_01/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version 
+//alert(3+3) we are using node.js, not browser
+
+//code readabilty should be high
+
+let name = "Dharmen"
+let age = 18
+let isLoggdIn = false
+
+//datatypes:
+
+//number
+//bigint
+//string -> ""
+//boolean -> true/false
+//null -> standalone value
+//undefined 
+//symbol -> unique
+//object
+
+console.log(typeof name);
+console.log(typeof age);
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
