9dad03ec1d8cc00dae44654f256f221c99b54ed2
KateliyaDharmen <kateliyadharmen333@gmail.com>
2024-01-12 21:20:31 +0530

    variables in js

---
+++ Basic_01/variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountName = "dhruvi"
+var accountPassword = 83749327
+acoountCity = "ahmedabad"
+
+
+// do not refer var because of block scope
+accountName = "dharmen"
+accountPassword = 93029
+accountCity = "heleo"
+let acccout;
+
+console.table([accountId,accountName,accountPassword,accountCity]);
