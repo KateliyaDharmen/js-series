7d34447f3519a7dc94f83ab9933d5ed288d8dfd7
KateliyaDharmen <kateliyadharmen333@gmail.com>
2024-01-13 22:05:53 +0530

    datatypes

--- Basic_01/datatypes.js
+++ Basic_01/datatypes.js
@@ -23,3 +23,34 @@ console.log(typeof age);
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+//datatypes summery
+
+//Primitive
+//7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+let nam = "dk"
+let value = 547;
+let isOk = true
+let userCode = null
+let userEmail;
+let userId = Symbol('123');
+let userPassword = Symbol('123')
+
+console.log(userId == userPassword); false
+
+let score = 564646464646464;
+//Reference(Non Primitive)
+//Array, Functions, Objects 
+
+let fruits = ["apple", "banana", "guava"];
+
+let myObj = {
+    name: "dharmen",
+    age: 18,
+    city: "Mumbai"
+}
+
+let myFunc = function myFunction() {
+    console.log("Hello js:)");
+}
