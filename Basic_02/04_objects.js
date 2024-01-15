e0c79210441ea307af5d41676d73f032f9eb9b08
KateliyaDharmen <kateliyadharmen333@gmail.com>
2024-01-15 17:41:40 +0530

    Objects part 2

---
+++ Basic_02/04_objects.js
@@ -0,0 +1,51 @@
+const appUser = new Object();
+
+appUser.id = "xyz123"
+appUser.email = "hello@gmail.com"
+appUser.isLoggedIn = false
+
+// console.log(appUser);
+
+const newAppUser = {
+    email: "new@gmail.com",
+    fullName: {
+        username:{
+            firstname: "Dharmen",
+            lastname: "Kateliya"
+        }
+    }
+}
+
+// console.log(newAppUser.fullName.username.firstname);
+
+//merge two objects
+const obj1 = {1: "one", 2: "two", 3: "three"}
+const obj2 = {4: "four", 5: "five", 6: "six"}
+
+// const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    }
+]
+
+// console.log(users[1].email);
+
+console.log(appUser);
+console.log(Object.keys(appUser));
+console.log(Object.values(appUser));
+console.log(Object.entries(appUser));
+console.log(appUser.hasOwnProperty("email"));
