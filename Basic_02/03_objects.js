2a04670be3db6026c7a448b9ca473c0f4923f706
KateliyaDharmen <kateliyadharmen333@gmail.com>
2024-01-15 16:47:16 +0530

    Objects Part 1

---
+++ Basic_02/03_objects.js
@@ -0,0 +1,37 @@
+//singletone
+//Object.create
+
+//object literals
+const mySymbol = Symbol("key1");
+const user1 = {
+    name: "Dharmen",
+    "full name": "dk",
+    [mySymbol]: "myKey1",
+    age: 20,
+    email: "dharmen@google.com",
+    isLoggedIn: true,
+    location: "ahmedabad",
+    lasLoginDays: ["monday", "sunday"],
+}
+
+// console.log(user1.name);
+// console.log(user1["email"]); 
+// console.log(user1["full name"]);
+//this are two types to access object
+
+console.log(user1[mySymbol]);
+
+user1.location = "mumbai"
+// Object.freeze(user1);
+user1.name = "dk";
+console.log(user1);
+
+user1.greeting = function () {
+    console.log("Hello js!!");
+}
+console.log(user1.greeting());
+
+user1.greeting2 = function () {
+    console.log(`Hello My Name is ${user1.name}`);
+}
+console.log(user1.greeting2());
