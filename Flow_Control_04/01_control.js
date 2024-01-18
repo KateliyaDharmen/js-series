b1ffc0af858d9bf8f28bf0b8ba3d99764ddb5610
KateliyaDharmen <kateliyadharmen333@gmail.com>
2024-01-18 20:51:23 +0530

    Control Flow in js

--- Flow_Control_04/01_control.js
+++ Flow_Control_04/01_control.js
@@ -6,3 +6,37 @@ if(age >=18){
 }else{
     console.log("teen");
 }
+
+if(2 != 3){
+    console.log("executed");
+}
+// >,<,<=,>=,!=, ==, === ,!==
+
+//if.....else if//
+const temp = "50"
+if(temp <= 22){
+    console.log("tempreture is cold");
+}else if(temp == 25)
+{
+    console.log("tempreture is moderated");
+}
+else if(temp > 30){
+    console.log("tempreture is hot");
+}
+else{
+    console.log("temp");
+}
+
+
+// && , ||
+const score = 2
+if(score < 50 && score >25){
+    console.log("average");
+}else if(score > 80 || score > 75)
+{
+    console.log("Good");
+}else {
+    console.log("vgood");
+}
+
+if(true) console.log("hello"),console.log("world"); // one line if statement
