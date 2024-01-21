0a50c6fdd3b0c13f6a7f67020fb331122f15197b
KateliyaDharmen <kateliyadharmen333@gmail.com>
2024-01-21 18:56:56 +0530

    loops

---
+++ Loops_05/04_highOrderLoops2.js
@@ -0,0 +1,13 @@
+// const coding = ["js", "c", "cpp", "php", "java"]
+
+// const result = coding.forEach((val) => {
+//     // console.log(val);
+//     return val
+// })
+
+// console.log(result); //it gives undefined because forEach loop does not return any values
+
+const myNum = [1,2,3,4,5,6,7,8,9,10]
+
+const result = myNum.filter((num) => num > 5 )
+console.log(result);
